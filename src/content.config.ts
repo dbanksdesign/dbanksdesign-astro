df3bb0c3ce90294c2de26a1508f0d4096f021cad
Danny Banks <djb@amazon.com>
2025-02-06 22:58:56 -0800

    third

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,41 +1,43 @@
-import { glob, file } from "astro/loaders";
-import { defineCollection, z } from "astro:content";
+import { glob, file } from 'astro/loaders';
+import { defineCollection, z } from 'astro:content';
 
 const speaking = defineCollection({
-  loader: file("src/data/speaking.json"),
-  schema: z.object({
-    title: z.string(),
-    event: z.string(),
-    eventLink: z.string(),
-    slideLink: z.string(),
-  }),
+	loader: file('src/data/speaking.json'),
+	schema: z.object({
+		title: z.string(),
+		event: z.string(),
+		eventLink: z.string(),
+		slideLink: z.string(),
+	}),
 });
 
 const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
 });
 
 const work = defineCollection({
-  // Load Markdown and MDX files in the `src/content/work/` directory.
-  loader: glob({ base: "./src/content/work", pattern: "**/*.{md,mdx}" }),
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    heroImage: z.string().optional(),
-    type: z.enum(["work", "project", "talk"]).optional(),
-  }),
+	// Load Markdown and MDX files in the `src/content/work/` directory.
+	loader: glob({ base: './src/content/work', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			pubDate: z.coerce.date(),
+			heroImage: image().optional(),
+			type: z.enum(['work', 'project', 'talk']).optional(),
+			order: z.number(),
+		}),
 });
 
 export const collections = { blog, speaking, work };
